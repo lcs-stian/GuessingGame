a890bc321fe8f3bb2ace7a46e8415df83c0c7848
Suzanne <STian@lcs.on.ca>
2021-11-30 14:26:58 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Suzanne Tian on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
